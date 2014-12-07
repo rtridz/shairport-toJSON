baf5e2c911f9a02a59518c350164886d485ce15f
Sergey Zakharchuk <rtridz@gmail.com>
2014-12-07 22:28:37 +0300

    now_playing_toJSON.sh

---
+++ now_playing_toJSON.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+ 
+now_playing_dir=/etc/now_playing
+datafile=$now_playing_dir.json
+ 
+stringify() {
+  a=${1%%=*}
+  b=${1##*=}
+  echo "\"$a\": \"$b\""
+}
+ 
+exec 4< $now_playing_dir
+while read -ru 4 line ; do
+  if [[ $line == artist* ]]; then
+    echo '{' > $datafile
+  elif [ -z "$line" ]; then
+    echo '}' >> $datafile
+    continue
+  fi
+ 
+  # jsonify the string
+  data=$(stringify "$line")
+ 
+  # for logging
+  echo $line
+ 
+  # pretty print the JSON
+  # comment is the last line of the output
+  if [[ $line == comment* ]]; then
+    echo "  $data" >> $datafile
+  else
+    echo "  $data," >> $datafile
+  fi
+done
+ 
+exec 4<&-
+echo Exiting
